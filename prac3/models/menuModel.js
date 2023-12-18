19720a40805c125e899c921e11cdaa17abc2eb56
wjdgns4070 <ljj888800@naver.com>
2023-12-18 13:40:32 +0900

    231218

--- prac3/models/menuModel.js
+++ prac3/models/menuModel.js
@@ -1,9 +1,31 @@
 // models/menuModel.js
 
 const menuItems = [
-    { id: '1', name: '피자', price: 21000 },
-    { id: '2', name: '햄버거', price: 18000 },
-    { id: '3', name: '샐러드', price: 8000 },
+    { id: '1', name: '페퍼로니 피자', price: 18000 },
+    { id: '2', name: '하와이안 피자', price: 17000 },
+    { id: '3', name: '불고기 피자', price: 15000 },
+    { id: '4', name: '통새우 피자', price: 17000 },
+    { id: '5', name: '치즈 피자', price: 13000 },
+    { id: '6', name: '스윗포테이토 피자', price: 15000 },
+    { id: '7', name: '샐러드', price: 8000 },
+    { id: '8', name: '샐러드', price: 8000 },
+    { id: '9', name: '샐러드', price: 8000 },
+    { id: '10', name: '샐러드', price: 8000 },
+    { id: '11', name: '샐러드', price: 8000 },
+    { id: '12', name: '샐러드', price: 8000 },
+    { id: '13', name: '샐러드', price: 8000 },
+    { id: '14', name: '샐러드', price: 8000 },
+    { id: '15', name: '샐러드', price: 8000 },
+    { id: '16', name: '샐러드', price: 8000 },
+    { id: '17', name: '샐러드', price: 8000 },
+    { id: '18', name: '샐러드', price: 8000 },
+    { id: '19', name: '샐러드', price: 8000 },
+    { id: '20', name: '샐러드', price: 8000 },
+    { id: '21', name: '샐러드', price: 8000 },
+    { id: '22', name: '샐러드', price: 8000 },
+    { id: '23', name: '샐러드', price: 8000 },
+
+
 ];
 
 function getMenuItems() {
--- prac3/public/js.js
+++ prac3/public/js.js
@@ -1,4 +1,25 @@
+// js.js 파일의 내용
 document.addEventListener("DOMContentLoaded", function () {
+
+    const cartButton = document.createElement('button');
+    cartButton.textContent = '장바구니 확인';
+    cartButton.style.backgroundColor = '#4CAF50';
+    cartButton.style.color = 'white';
+    cartButton.style.padding = '15px 30px';  
+    cartButton.style.border = 'none';
+    cartButton.style.borderRadius = '20px';  //
+    cartButton.style.cursor = 'pointer';
+
+    // 사이드바 하단 가운데에 위치시키기
+    cartButton.style.position = 'fixed';
+    cartButton.style.bottom = '10px';
+    cartButton.style.left = '85%';
+    cartButton.style.transform = 'translateX(-50%)';
+    cartButton.style.zIndex = '100'
+
+    cartButton.addEventListener('click', showCart);
+    document.body.appendChild(cartButton);
+
     const menuButtons = document.querySelectorAll('.menu-button');
 
     menuButtons.forEach(menuButton => {
@@ -21,77 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
         alert('장바구니에 추가되었습니다!');
     }
 
-    // 장바구니 확인 버튼 추가
-    const cartButton = document.createElement('button');
-    cartButton.textContent = '장바구니 확인';
-    cartButton.addEventListener('click', showCart);
-    document.body.appendChild(cartButton);
-
-    function showCart() {
-        const cartCookie = document.cookie.split('; ')
-            .find(cookie => cookie.startsWith('cart='));
-
-        if (!cartCookie) {
-            alert('장바구니가 비어 있습니다.');
-            return;
-        }
-
-        const decodedCartValue = decodeURIComponent(cartCookie.split('=')[1]);
-        const cartItems = JSON.parse(decodedCartValue);
-
-        const totalAmount = cartItems.reduce((sum, item) => sum + parseFloat(item.price), 0);
-
-        const popupContent = cartItems.map(item => `${item.name} - $${parseFloat(item.price).toFixed(2)}`).join('<br>');
-        const popupHTML = `
-            <div>
-                <h2>장바구니 내역</h2>
-                <p>${popupContent}</p>
-                <hr>
-                <p><strong>총 금액:</strong> $${totalAmount.toFixed(2)}</p>
-            </div>
-        `;
-
-        const popup = window.open('', 'cartPopup', 'width=400,height=400');
-        popup.document.body.innerHTML = popupHTML;
-
-        // "구매하기" 버튼 추가
-        const purchaseButton = document.createElement('button');
-        purchaseButton.textContent = '구매하기';
-        purchaseButton.addEventListener('click', purchaseItems);
-        popup.document.body.appendChild(purchaseButton);
-    }
-
     // 클라이언트 측 코드
-function purchaseItems() {
-    const cartCookie = document.cookie.split('; ')
-        .find(cookie => cookie.startsWith('cart='));
-
-    if (!cartCookie) {
-        alert('장바구니가 비어 있습니다.');
-        return;
-    }
-
-    const decodedCartValue = decodeURIComponent(cartCookie.split('=')[1]);
-    const cartItems = JSON.parse(decodedCartValue);
-
-    // 배열로 감싸서 서버로 전송
-    axios.post('/purchase', cartItems)
-        .then(response => {
-            console.log('Purchase successful:', response.data);
-
-            // 구매가 완료되면 쿠키 초기화
-            document.cookie = 'cart=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-
-            // 팝업 창 닫기
-            window.location.reload();
-        })
-        .catch(error => {
-            console.error('Purchase error:', error);
-            alert('구매 중 오류가 발생했습니다.');
-        });
-}
-
-
     function getCookie(name) {
         const value = `; ${document.cookie}`;
         const parts = value.split(`; ${name}=`);
